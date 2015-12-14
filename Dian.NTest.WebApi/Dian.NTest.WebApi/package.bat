db0b4eae2f6de02bfc2c2022859f69c6b8e4fd04
mybbcat <mybbcat@gmail.com>
2015-12-14 16:05:47 +0800

    添加至nuget。

--- Dian.NTest.WebApi/Dian.NTest.WebApi/package.bat
+++ Dian.NTest.WebApi/Dian.NTest.WebApi/package.bat
@@ -1,5 +1,5 @@
-nuget pack Dian.NLogEx.csproj
+nuget pack Dian.NTest.WebApi.csproj
 ping /n 2 127.1 >nul
 nuget setapikey e69a7d51-8f6f-495f-82ec-50d0db75cf42
 ping /n 2 127.1 >nul
-nuget push Dian.NLogEx*.nupkg
+nuget push Dian.NTest.WebApi*.nupkg
